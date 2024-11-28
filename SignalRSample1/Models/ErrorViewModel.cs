ffd63cd02e747a6476def4067f60709eda8d0adf
Sreehari <sreeharins001@gmail.com>
2024-11-28 19:48:04 +0530

    Add project files.

---
+++ SignalRSample1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRSample1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
